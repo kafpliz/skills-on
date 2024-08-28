45f5f225c709c3391f3e60ea796b5be2b5410b7e
kafpliz <kafpliz@gmail.com>
2024-08-28 03:40:30 +0300

    начал разработку auth-page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { AuthComponent } from './components/auth/auth.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'auth', component: AuthComponent }
+];
